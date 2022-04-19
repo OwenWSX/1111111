268f07be2c51e993fd2f726b5270887d997ff322
OwenWSX <103395292+OwenWSX@users.noreply.github.com>
2022-04-19 18:30:36 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function myFunction() {
+        var x = document.getElementById("myText").value;
+        document.getElementById("demo").innerHTML =("welcome"+ x);
+      }
+
+
+
+
+
+
+
+function myFunction() {
+        var x = document.getElementById("myText").value;
+        document.getElementById("demo").innerHTML =("welcome"+ x);
+      }
+    
+      function calc(func){
+        var result = document.getElementById("result");
+        var num1 = parseInt(document.getElementById("num1").value);
+        var num2 = parseInt(document.getElementById("num2").value);
+        if(isNaN(num1) || isNaN(num2)){
+            alert("Please enter a number");
+            return false;
+        }
+        result.value = func(num1,num2);
+    }
+    function Try(num1,num2){
+        return num1+num2;
+
+    }
